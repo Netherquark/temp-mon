8f82fe2cd2b0cf2d317dec6bae5b12505acdfea8
Netherquark <netherquark@protonmail.com>
2023-06-13 12:10:08 +0530

    fix normalisation (notime)

--- heat_refined_jetson_normalisation.cpp
+++ heat_refined_jetson_normalisation.cpp
@@ -11,15 +11,8 @@ int main()
 {
       int i = 0;
       while (i < 1001){
-        auto start = std::chrono::high_resolution_clock::now();
-
-      auto stop = std::chrono::high_resolution_clock::now();
-  
-      auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);  
       
       system("sudo timeout 0.06s sudo tegrastats --interval 0.001");
-
-      std::cout<< std::fixed << std::setprecision(9) << duration.count() << std::endl;
       
       i++;
       }
--- heat_refined_x86_normalisation.cpp
+++ heat_refined_x86_normalisation.cpp
@@ -11,15 +11,8 @@ int main()
 {
       int i = 0;
       while (i < 1001){
-        auto start = std::chrono::high_resolution_clock::now();
-
-      auto stop = std::chrono::high_resolution_clock::now();
-  
-      auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);  
       
-      system("sudo turbostat --Summary --interval 0.001 --quiet --num_iterations 1 --hide \"Avg_MHz\" --hide \"Busy%\" --hide \"Bzy_MHz\" --hide \"TSC_MHz\" --hide \"IPC\" --hide \"IRQ\" --hide \"POLL\" --hide \"C1\" --hide \"C2\" --hide \"C3\" --hide \"POLL%\" --hide \"C1%\" --hide \"C2%\" --hide \"C3%\" --hide \"PkgWatt\"");
-
-      std::cout<< std::fixed << std::setprecision(9) << duration.count() << std::endl;
+        system("sudo turbostat --Summary --interval 0.001 --quiet --num_iterations 1 --hide \"Avg_MHz\" --hide \"Busy%\" --hide \"Bzy_MHz\" --hide \"TSC_MHz\" --hide \"IPC\" --hide \"IRQ\" --hide \"POLL\" --hide \"C1\" --hide \"C2\" --hide \"C3\" --hide \"POLL%\" --hide \"C1%\" --hide \"C2%\" --hide \"C3%\" --hide \"PkgWatt\"");
       
       i++;
       }
